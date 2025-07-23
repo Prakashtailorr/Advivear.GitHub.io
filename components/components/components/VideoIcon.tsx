9374685a4f369aa106182690a7f8c385905e851e
Prakashtailorr <shawamimohit@gmail.com>
2025-07-23 11:52:07 +0530

    Create VideoIcon.tsx

---
+++ components/components/components/VideoIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const VideoIcon = () => {
+  return (
+    <svg
+      className="w-6 h-6 text-red-500"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14m-6 2h5a2 2 0 002-2V8a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2z"
+      />
+    </svg>
+  );
+};
+
+export default VideoIcon;
